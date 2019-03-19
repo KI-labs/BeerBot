75048606dce9d929dedd75f0de49a9108272a8a0
m.fuchs <m.fuchs@kigroup.de>
2019-03-19 15:49:35 +0100

    Add door state detection

--- beerbot.py
+++ beerbot.py
@@ -1,21 +1,29 @@
+import os
+import time
+from os import getenv
+
+from dotenv import load_dotenv
 from util.image_utils import is_door_closed
+from image_processing.utils import take_picture
 
-print (is_door_closed())
+load_dotenv()
 
-event = False
+current_door_state = "closed"
+prev_door_state = "open"
 
-# while True:
-#
-#     # TODO low res photo
-#     # take_picture(out_dir='data/temp')
-#
-#     # TODO check logic for event
-#     if event:
-#         # take_picture(q='high', out_fmt='png', out_dir='data/raw')
-#
-#         # TODO -> find bottles save to data/processed
-#
-#         # TODO -> build inventory data/inventory
-#
-#     time.sleep(5)
-#
+while True:
+    take_picture(q="low", out_dir="{}/temp".format(os.getenv("DATA_DIR")))
+
+    prev_door_state = current_door_state
+    current_door_state = "closed" if is_door_closed() else "open"
+
+    if current_door_state == "closed" and prev_door_state == "open":
+        take_picture(
+            q="high", out_fmt="png", out_dir="{}/raw".format(os.getenv("DATA_DIR"))
+        )
+
+        # TODO -> find bottles save to data/processed
+
+        # TODO -> build inventory data/inventory
+
+    time.sleep(5)
