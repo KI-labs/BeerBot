fc1b680d4d3d42c3c592606021d5582164cf0089
Daniel Cachapa <d.cachapa@kigroup.de>
2019-03-20 10:41:12 +0100

    Revert "Improve door state detection"
    This reverts commit 8ddde8d9f5753bab637d4803435bbed9268ce51f.

--- beerbot.py
+++ beerbot.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 
 from image_processing.find_bottles import find_bottles
 from image_processing.utils import take_picture, update_inventory
-from util.image_utils import compare_images
+from util.image_utils import is_door_open
 
 load_dotenv()
 
-was_door_open = False
+current_door_state = "closed"
+prev_door_state = "open"
 
 # set template
 print('creating template')
@@ -22,19 +23,14 @@ while True:
                             out_dir="{}/temp".format(os.getenv("DATA_DIR")))
     file_path = os.path.join(os.getenv("DATA_DIR"), 'temp', file_out)
 
-    print('Checking template…')
-    template_diff = compare_images(template_out, file_out)
-    print("Difference: {0:.2f}%".format(template_diff * 100))
+    print('Checking template')
 
-    # prev_door_state = current_door_state
-    # current_door_state = "open" if is_door_open(template_path, file_path) else "closed"
-    # print("Current_door_state: {}".format(current_door_state))
-    # print("Prev_door_state: {}".format(prev_door_state))
+    prev_door_state = current_door_state
+    current_door_state = "open" if is_door_open(template_path, file_path) else "closed"
+    print("Current_door_state: {}".format(current_door_state))
+    print("Prev_door_state: {}".format(prev_door_state))
 
-    if not was_door_open and template_diff > 0.05:
-        print('Door opened')
-        was_door_open = True
-
+    if current_door_state == "closed" and prev_door_state == "open":
         print('Taking photo for analysis')
         file_out = take_picture(
             q="high", out_fmt="png", sleep_time=1, out_dir="{}/raw".format(os.getenv("DATA_DIR"))
@@ -59,9 +55,5 @@ while True:
         print('updating inventory at {} with {} bottles'.format(tstamp, num))
 
         update_inventory("{}/inventory.txt".format(os.getenv("DATA_DIR")), tstamp, num)
-
-    if was_door_open and template_diff > 0.20:
-        print('Door closed')
-        was_door_open = False
 
     time.sleep(2)
